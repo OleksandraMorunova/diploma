721681b2af373a0bcc33d36c77b185b5f7120806
OleksandraMorunova <oleksandramorunova@gmail.com>
2023-03-05 09:18:43 +0200

    77

---
+++ AssistantForPsychologist/app/src/main/java/com/example/projectfordiploma/ui/login/LoginResult.java
@@ -0,0 +1,28 @@
+package com.example.projectfordiploma.ui.login;
+
+import androidx.annotation.Nullable;
+
+class LoginResult {
+    @Nullable
+    private LoggedInUserView success;
+    @Nullable
+    private Integer error;
+
+    LoginResult(@Nullable Integer error) {
+        this.error = error;
+    }
+
+    LoginResult(@Nullable LoggedInUserView success) {
+        this.success = success;
+    }
+
+    @Nullable
+    LoggedInUserView getSuccess() {
+        return success;
+    }
+
+    @Nullable
+    Integer getError() {
+        return error;
+    }
+}
